230e5efb2d34321a1be0feff57b96898c26d6e1b
Rob Donnelly <Robert.Donnelly@jpl.nasa.gov>
2026-03-23 10:18:13 -0700

    Make it work

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 from playwright.sync_api import sync_playwright
 
-from pprint import pp
+import sys
+import yaml
 
 def main():
+    url = sys.argv[1]
+
     with sync_playwright() as p:
         browser = p.chromium.launch(channel="chrome", headless=False)
-        context = browser.new_context()
+        context = browser.new_context(viewport={"width": 500, "height": 700})
         page = context.new_page()
-        page.goto("https://login.microsoftonline.com/")
-
-        username = page.get_by_role("textbox", name="Enter your email, phone, or")
-        username.click()
-        username.fill("rfdonnelly@gmail.com")
-        page.get_by_role("button", name="Next").click()
-        page.pause()
-
-        pp(context.cookies())
-
+        page.goto(url)
+        page.wait_for_url(url)
+        print(yaml.dump(context.cookies()))
 
 if __name__ == "__main__":
     main()
